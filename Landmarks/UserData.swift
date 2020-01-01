e2c63ad5107ecdf2d85b6f50ea122a10a4528dfb
h-ueno2 <menbou1212@gmail.com>
2020-01-01 12:02:16 +0900

    Initial Commit

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,14 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by ueno hiroki on 2020/01/01.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    
+}
